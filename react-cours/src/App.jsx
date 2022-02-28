ec198a9830b0c0a7432e559109b5f34cce609ac7
HugoReboulet <hugo.reboulet@outlook.fr>
2022-02-28 15:29:03 +0100

    mise en place du routeur

---
+++ react-cours/src/App.jsx
@@ -0,0 +1,20 @@
+import { Outlet, Link } from "react-router-dom";
+
+// page d'acceuil contenant les liens vers les 2 autres pages
+export default function App() {
+  return (
+    <div>
+      <h1>Projet React</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/page1">Page1</Link> |{" "}
+        <Link to="/page2">Page2</Link>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
