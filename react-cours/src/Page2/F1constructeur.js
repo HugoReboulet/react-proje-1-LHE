6045a4ec05a7f05ab4aa2192c5489b2323b624b0
ebasson <esteban.basson@pwa-assurance.fr>
2022-03-01 09:15:42 +0100

    commit travail hier soir

---
+++ react-cours/src/Page2/F1constructeur.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import F1constructeur from "./Page2/F1-constructeur";
+
+
+const App = () => {
+    const [mesConstruteurs, setMesConstructeurs] = useState([])
+
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    async function getData() {
+        const response = await fetch("http://ergast.com/api/f1/2021/constructors.json")
+        const data = await response.json()
+        setMesConstructeurs(data.MRData.ConstructorTable.Constructors)
+        console.log(mesConstruteurs)
+    }
+
+    getData()
+
+    return (
+        <div>
+            <F1constructeur nomConstructeur={mesConstruteurs} origine={'Pays'}/>
+
+            {mesConstruteurs.map((constructeur) => {return <div key={constructeur.constructorId}>{constructeur.constructorId}</div>})}
+        </div>
+    )
+}
+
+export default App
